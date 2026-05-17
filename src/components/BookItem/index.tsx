287fa92d2369c74abf8cb08a38dae0d3f97d7ab0
Likhith RK <likhithraj10@gmail.com>
2026-05-17 22:51:16 +0530

    added lazy loading to bookitem cover photos

--- src/components/BookItem/index.tsx
+++ src/components/BookItem/index.tsx
@@ -4,26 +4,25 @@ import type { Book } from "../../context/types";
 import { BOOK_COVER_PREFIX } from "../../utils/constants";
 
 interface BookItemProps {
-	book: Book;
-	onClick: React.MouseEventHandler<HTMLDivElement>;
+  book: Book;
+  onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
 const BookItem: React.FC<BookItemProps> = (props) => {
-	const { book, onClick } = props;
-	return (
-		<div
-			className='book-item'
-			onClick={onClick}>
-			<div className='cover-image'>
-				<img
-					src={`${BOOK_COVER_PREFIX}${book.image}`}
-					alt={book.name}
-				/>
-			</div>
-			<div className='author-name'>{book.author.name}</div>
-			<div className='title'>{book.name}</div>
-		</div>
-	);
+  const { book, onClick } = props;
+  return (
+    <div className="book-item" onClick={onClick}>
+      <div className="cover-image">
+        <img
+          loading="lazy"
+          src={`${BOOK_COVER_PREFIX}${book.image}`}
+          alt={book.name}
+        />
+      </div>
+      <div className="author-name">{book.author.name}</div>
+      <div className="title">{book.name}</div>
+    </div>
+  );
 };
 
 export default BookItem;
